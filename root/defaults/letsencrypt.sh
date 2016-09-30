1f814fbc251d7240ebfac8744d024c120c14a36b
zaggash <zaggash@users.noreply.github.com>
2016-09-30 10:40:57 +0200

    review date function

--- root/defaults/letsencrypt.sh
+++ root/defaults/letsencrypt.sh
@@ -19,9 +19,9 @@ certificate_needs_update() {
         cert_folder="$1"
         main_cert_name="$2"
         max_days_to_expiration="$3"
-        exp_time=$(date -D "`openssl x509 -in $cert_folder/$main_cert_name/$main_cert_name.pem -text -noout | grep 'Not After' | cut -c 25-`" +%s)
-        now=$(date -D "now" +%s)
-        days_to_exp=$(echo \( $exp_time - $now \) / 86400 | bc)
+        exp_time=$(date -d "`openssl x509 -in $cert_folder/$main_cert_name/$main_cert_name.pem -text -noout | grep 'Not After' | cut -c 25-`" +%s)
+        now=$(date -d "now" +%s)
+        let "days_to_exp=$exp_time-$now" && let "days_to_exp=$days_to_exp/86400"
         if [ "$days_to_exp" -gt "$max_days_to_expiration" ]; then
                 return 1
         else
